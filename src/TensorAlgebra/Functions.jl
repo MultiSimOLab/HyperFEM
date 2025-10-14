878ca5b70eac361ba2168e991aef07f08af1d25f
Miguel Masó <miguel.maso@upc.edu>
2025-10-14 17:41:27 +0200

    algebraic operations

--- src/TensorAlgebra/Functions.jl
+++ src/TensorAlgebra/Functions.jl
@@ -21,6 +21,18 @@ function cof(A::TensorValue)
 end
 
 
+"""
+Jacobian regularization
+"""
+function logreg(J; Threshold=0.01)
+  if J >= Threshold
+    return log(J)
+  else
+    return log(Threshold) - (3.0 / 2.0) + (2 / Threshold) * J - (1 / (2 * Threshold^2)) * J^2
+  end
+end
+
+
 function _∂H∂F_2D()
   TensorValue(0.0, 0.0, 0.0, 1.0, 0.0, 0.0, -1.0, 0.0, 0.0, -1.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0)
 end
@@ -28,4 +40,13 @@ end
 
 function trAA(A::TensorValue{3, 3, T, N}) where {T, N}
   return sum(A.data[i]*A.data[i] for i in 1:N)
+end
+
+
+@generated function Tensorize(A::VectorValue{D,Float64}) where {D}
+  str = ""
+  for i in 1:D
+    str *= "A.data[$i], "
+  end
+  Meta.parse("TensorValue($str)")
 end
