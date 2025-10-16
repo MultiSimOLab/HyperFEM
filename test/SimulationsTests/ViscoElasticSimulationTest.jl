62f9260ab7ab490b906d65fef77d9acf706cfaa1
Miguel Masó <miguel.maso@upc.edu>
2025-10-16 12:15:59 +0200

    unused

--- test/SimulationsTests/ViscoElasticSimulationTest.jl
+++ test/SimulationsTests/ViscoElasticSimulationTest.jl
@@ -6,9 +6,3 @@ include(filename)
 λx, σΓ = visco_elastic_simulation(t_end=5, write_vtk=false, verbose=false)
 
 @test σΓ[end] ≈ 152821.386
-
-if Int(VERSION.minor) > 10
-  @test σΓ[end] ≈ 152821.386
-else
-  @test ≈(σΓ[end], 152821.386, rtol=1e-6)
-end
