3926a72a432fd877a79ece67b37bc3e7887f13de
Miguel Masó <miguel.maso@upc.edu>
2025-11-04 12:00:21 +0100

    no verbose

--- benchmark/SimulationsBenchmarks/ViscoElasticSimulationBenchmark.jl
+++ benchmark/SimulationsBenchmarks/ViscoElasticSimulationBenchmark.jl
@@ -3,4 +3,4 @@ BASE_FOLDER = dirname(dirname(pathof(HyperFEM)))
 filename = joinpath(BASE_FOLDER, "test/data/ViscoElasticSimulation.jl")
 include(filename)
 
-SUITE["Simulations"]["ViscoElastic"] = @benchmarkable visco_elastic_simulation(writevtk=false)
+SUITE["Simulations"]["ViscoElastic"] = @benchmarkable visco_elastic_simulation(writevtk=false, verbose=false)
