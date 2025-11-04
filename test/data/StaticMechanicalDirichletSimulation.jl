11586384dc7b5336492ddf3de0da166764ec9301
Miguel Masó <miguel.maso@upc.edu>
2025-11-04 11:47:40 +0100

    dirichlet test file

---
+++ test/data/StaticMechanicalDirichletSimulation.jl
@@ -0,0 +1,84 @@
+using Gridap, GridapGmsh, GridapSolvers, DrWatson
+using GridapSolvers.NonlinearSolvers
+using GridapSolvers.LinearSolvers
+using TimerOutputs
+using Gridap.FESpaces
+using HyperFEM
+using HyperFEM.ComputationalModels.CartesianTags
+
+
+function main(;writevtk=true, verbose=true)
+
+  pname = "Stretch"
+  
+  long   = 0.05   # m
+  width  = 0.005  # m
+  thick  = 0.002  # m
+  geometry = CartesianDiscreteModel((0, long, 0, width, 0, thick), (5,2,2))
+  labels = get_face_labeling(geometry)
+  add_tag_from_tags!(labels, "fixed", CartesianTags.faceX0)
+  add_tag_from_tags!(labels, "moving", CartesianTags.faceX1)
+
+  simdir = datadir("sims", pname)
+  setupfolder(simdir)
+    
+  physmodel = MooneyRivlin3D(λ=3.0, μ1=1.0, μ2=0.0, ρ=1.0)
+
+  # Setup integration
+  order = 1
+  degree = 2 * order
+  Ω = Triangulation(geometry)
+  dΩ = Measure(Ω, degree)
+
+  # Dirichlet boundary conditions
+  dir_u_tags = ["fixed", "moving"]
+  dir_u_values = [[0.0, 0.0, 0.0], [0.08, 0.0, 0.0]]
+  dir_u_timesteps = [Λ -> 1.0, Λ -> Λ]
+  D_bc = DirichletBC(dir_u_tags, dir_u_values, dir_u_timesteps)
+
+  #  FE spaces
+  reffeu = ReferenceFE(lagrangian, VectorValue{3,Float64}, order)
+
+  V = TestFESpace(Ω, reffeu, D_bc, conformity=:H1)
+  U = TrialFESpace(V, D_bc, 0.0)
+
+  #  residual and jacobian function of load factor
+  res(Λ) = (u, v) -> residual(physmodel, u, v, dΩ)
+  jac(Λ) = (u, du, v) -> jacobian(physmodel, u, du, v, dΩ)
+
+  #computational model
+  ls = LUSolver()
+  nls_ = NewtonSolver(ls; maxiter=15, rtol=1.e-12, verbose=verbose)
+
+  comp_model = StaticNonlinearModel(res, jac, U, V, D_bc; nls=nls_)
+
+  function driverpost_mech(post)
+    if writevtk
+      state = post.comp_model.caches[3]
+      Λ_ = post.iter
+      Λ = post.Λ[Λ_]
+      xh = FEFunction(U, state)
+      pvd = post.cachevtk[3]
+      filePath = post.cachevtk[2]
+      if post.cachevtk[1]  
+        Λstring = replace(string(round(Λ, digits=2)), "." => "_")
+        pvd[Λ_] = createvtk(Ω,
+          filePath *  "/" * pname * "_Λ_" * Λstring * ".vtu",
+          cellfields=["u" => xh])
+      end
+    end
+  end
+
+  post_model = PostProcessor(comp_model, driverpost_mech; is_vtk=writevtk, filepath=simdir)
+
+  @timeit pname begin
+    x, flag = solve!(comp_model; stepping=(nsteps=10, maxbisec=10), post=post_model)
+  end
+end
+
+
+if abspath(PROGRAM_FILE) == @__FILE__
+  reset_timer!()
+  main()
+  print_timer()
+end
