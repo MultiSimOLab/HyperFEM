84db157ecb8a8f6b7e7c4cf26b4f7a5ea2152757
Miguel Masó <miguel.maso@upc.edu>
2025-11-06 12:43:12 +0100

    Faster visco-elastic simulation

--- benchmark/SimulationsBenchmarks/ViscoElasticSimulationBenchmark.jl
+++ benchmark/SimulationsBenchmarks/ViscoElasticSimulationBenchmark.jl
@@ -2,4 +2,4 @@
 filename = projdir("test/data/ViscoElasticSimulation.jl")
 include(filename)
 
-SUITE["Simulations"]["ViscoElastic"] = @benchmarkable visco_elastic_simulation(writevtk=false, verbose=false)
+SUITE["Simulations"]["ViscoElastic"] = @benchmarkable visco_elastic_simulation(t_end=2, writevtk=false, verbose=false)
--- test/SimulationsTests/ViscoElasticSimulationTest.jl
+++ test/SimulationsTests/ViscoElasticSimulationTest.jl
@@ -2,6 +2,6 @@
 filename = projdir("test/data/ViscoElasticSimulation.jl")
 include(filename)
 
-λx, σΓ = visco_elastic_simulation(t_end=5, writevtk=false, verbose=false)
+λx, σΓ = visco_elastic_simulation(t_end=2, writevtk=false, verbose=false)
 
-@test σΓ[end] ≈ 152821.386
+@test σΓ[end] ≈ 80903.472
