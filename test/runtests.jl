37e2441fcde21b934b186b875122478d5f00d322
Jesus Martínez <jesus.martinez@upct.es>
2025-11-20 11:07:38 +0100

    test

--- test/runtests.jl
+++ test/runtests.jl
@@ -6,26 +6,18 @@ using Test
 @testset "HyperFEMTests" verbose = true begin
 
   include("TestConstitutiveModels/runtests.jl")
-
-  include("TestTensorAlgebra/runtests.jl")
-
-  include("TestWeakForms/runtests.jl")
-
-  include("SimulationsTests/runtests.jl")
-
-end;
-
-@show "Second round"
-
-
-@testset "HyperFEMTests" verbose = true begin
-
   include("TestConstitutiveModels/runtests.jl")
 
   include("TestTensorAlgebra/runtests.jl")
+  include("TestTensorAlgebra/runtests.jl")
 
+  include("TestWeakForms/runtests.jl")
   include("TestWeakForms/runtests.jl")
 
   include("SimulationsTests/runtests.jl")
+  include("SimulationsTests/runtests.jl")
 
 end;
+
+
+ 
