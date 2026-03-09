09bfb3d22480e74c7e30bbb312b59726e2234efe
Miguel Masó <miguel.maso@upc.edu>
2026-03-09 17:08:35 +0100

    runtest

--- test/SimulationsTests/runtests.jl
+++ test/SimulationsTests/runtests.jl
@@ -10,6 +10,8 @@ using Test
 
     include("StaticMechanicalNeumannTest.jl")
 
+    include("StaggeredElectroMechanicalTest.jl")
+
     # include("Stokes.jl")
 
     # include("BoundaryConditions.jl")
