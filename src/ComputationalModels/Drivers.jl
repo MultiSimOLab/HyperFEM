cbc3d505f9548b22b687eb0f9afc91b498060211
miguelmaso <miguel.maso@upc.edu>
2026-03-09 18:18:34 +0100

    try fix warning

--- src/ComputationalModels/Drivers.jl
+++ src/ComputationalModels/Drivers.jl
@@ -23,9 +23,9 @@ struct StaggeredModel{A,B,C,D} <: ComputationalModel
     caches::D
 
     function StaggeredModel(
-        CompModels::Tuple{Vararg{<:ComputationalModel,N}},
-        state⁺::Tuple{Vararg{<:FEFunction,N}},
-        state⁻::Tuple{Vararg{<:FEFunction,N}}) where N
+        CompModels::Tuple{Vararg{ComputationalModel,N}},
+        state⁺::Tuple{Vararg{FEFunction,N}},
+        state⁻::Tuple{Vararg{FEFunction,N}}) where N
 
         x⁺ = map((x) -> get_free_dof_values(x), state⁺)
         x⁻ = map((x) -> get_free_dof_values(x), state⁻)
