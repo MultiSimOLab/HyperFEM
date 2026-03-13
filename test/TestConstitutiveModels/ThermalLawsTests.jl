8dc8eda10b3af0bf67cbed123a15d097288bda69
Miguel Masó <miguel.maso@upc.edu>
2026-03-13 14:33:37 +0100

    better tolerance

--- test/TestConstitutiveModels/ThermalLawsTests.jl
+++ test/TestConstitutiveModels/ThermalLawsTests.jl
@@ -7,6 +7,6 @@ using Test
   f, df, ddf = derivatives(law)
   for θ ∈ 200.0:50:400  # NOTE: The numerical derivative of erf is a bad approximation, while the analyitical function uses the exact value.
     @test isapprox(df(θ), ForwardDiff.derivative(f, θ), rtol=1e-3)
-    @test isapprox(ddf(θ), ForwardDiff.derivative(df, θ), rtol=1e-3)
+    @test isapprox(ddf(θ), ForwardDiff.derivative(df, θ), rtol=1e-10)
   end
 end
