b9361c624aef0066371b891535ceabfb036a8097
Miguel Masó <miguel.maso@upc.edu>
2026-05-21 15:25:26 +0200

    Fix HyperFEM distributed compilation

--- ext/ComputationalModels/ComputationalModels.jl
+++ ext/ComputationalModels/ComputationalModels.jl
@@ -1,11 +1,25 @@
 module ComputationalModels
 
 using HyperFEM
-using Gridap, GridapDistributed, GridapPETSc
+using Gridap, Gridap.Algebra, GridapDistributed, GridapPETSc
 using PartitionedArrays
 using PartitionedArrays: getany
 
-include("FESpaces.jl")
-include("GridapExtras.jl")
+
+function HyperFEM.ComputationalModels.instantiate_caches(x, nls::PETScNonlinearSolver, op::NonlinearOperator)
+  return GridapPETSc._setup_cache(x, nls, op)
+end
+
+function HyperFEM.ComputationalModels.get_local_matrix_type(a::GridapDistributed.DistributedSparseMatrixAssembler)
+  return getany(map(get_matrix_type,a.assems))
+end
+
+function HyperFEM.ComputationalModels.get_local_vector_type(a::GridapDistributed.DistributedSparseMatrixAssembler)
+  return getany(map(get_vector_type,a.assems))
+end
+
+function HyperFEM.ComputationalModels.get_local_assembly_strategy(a::GridapDistributed.DistributedSparseMatrixAssembler)
+  return get_assembly_strategy(a)
+end
 
 end
